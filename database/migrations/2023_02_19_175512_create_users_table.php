de039c280be3939438d458b77b7e1c5c28c2df89
VelizarYordanov <velizariordanov75@gmail.com>
2023-02-19 20:07:29 +0200

    db changes

--- database/migrations/2023_02_19_175512_create_users_table.php
+++ database/migrations/2023_02_19_175512_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            $table->primary(['id']);
+            //$table->primary(['id']);
         });
     }
 
