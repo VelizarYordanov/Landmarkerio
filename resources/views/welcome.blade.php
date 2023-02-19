da5ac385cf5bbd645cdeabd41a1be141b8df059e
VelizarYordanov <velizariordanov75@gmail.com>
2023-02-19 20:25:33 +0200

    tailwind test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @vite(['resources/js/welcome.js'])
 @section('content')
-<div class="container">
+<div class="container bg-red-900">
     <div class="row justify-content-center">
         <div class="col-md-8">
         <form id="input-form">
